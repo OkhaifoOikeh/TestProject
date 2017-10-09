c2415ba323ee558bc798ab60dabab24985a4d856
OkhaifoOikeh <okhaifooikeh@gmail.com>
2017-10-09 18:03:35 -0400

    fortest
    testing

---
+++ src/com/testversion/main.java
@@ -0,0 +1,15 @@
+package com.testversion;
+
+
+public class main {
+
+	public static void main(String[] args) {
+		Calculator(1,2);
+	}
+	
+	
+	public static void Calculator(int val1, int val2){
+		System.out.println("Addition: "+(val1+val2));
+	}
+
+}

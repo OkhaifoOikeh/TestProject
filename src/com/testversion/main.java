1517e819969219bd3be982b198bb5af60772368e
OkhaifoOikeh <okhaifooikeh@gmail.com>
2017-10-10 00:27:54 -0400

    noew
    something

--- src/com/testversion/main.java
+++ src/com/testversion/main.java
@@ -9,8 +9,9 @@ public class main {
 	
 	
 	public static void Calculator(int val1, int val2){
-		System.out.println("Addition: "+(val1+val2));
 		System.out.println("Sub: "+(val1 - val2));
+		System.out.println("Mul: "+(val1 * val2));
+		System.out.println("Divide "+(val1 / val2));
 	}
 
 }

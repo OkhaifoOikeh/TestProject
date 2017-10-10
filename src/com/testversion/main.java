b968ce65cc4ef9192fa48296f22fb342ca812596
OkhaifoOikeh <okhaifooikeh@gmail.com>
2017-10-10 00:44:26 -0400

    commiter
    here is another one

--- src/com/testversion/main.java
+++ src/com/testversion/main.java
@@ -5,13 +5,18 @@ public class main {
 
 	public static void main(String[] args) {
 		Calculator(1,2);
+		Calculator(2,4);
 	}
 	
 	
 	public static void Calculator(int val1, int val2){
+		System.out.println("Addition: "+(val1+val2));
 		System.out.println("Sub: "+(val1 - val2));
-		System.out.println("Mul: "+(val1 * val2));
-		System.out.println("Divide "+(val1 / val2));
+		System.out.println("Multiply: "+(val1*val2));
+	}
+	
+	public static int Stuff(){
+		System.out.println("I am here");
 	}
 
 }
